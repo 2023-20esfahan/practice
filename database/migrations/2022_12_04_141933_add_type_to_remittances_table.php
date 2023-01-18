1b5e69fb8a218499a2dc4b08598468759fceb5d3
Abbass <abbassmortazavi@gmail.com>
2023-01-18 23:11:02 +0330

    add files

--- database/migrations/2022_12_04_141933_add_type_to_remittances_table.php
+++ database/migrations/2022_12_04_141933_add_type_to_remittances_table.php
@@ -13,14 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::table('remittances', function (Blueprint $table) {
+            $table->enum('type',['1', '2', '3', '4', '5'])->default('1')->after('freight_pic');
+            $table->string('date')->nullable()->after('description');
         });
     }
 
@@ -31,6 +26,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::table('remittances', function (Blueprint $table) {
+            $table->dropColumn('type');
+        });
     }
 };
